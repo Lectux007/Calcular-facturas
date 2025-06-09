5a212d8d42c14aff8455e3e6a47ab904ec33513e
Lectux007 <jhonnatan.01.02.03@gmail.com>
2025-06-08 21:40:17 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,36 @@
-const CACHE_NAME = 'v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js'
-  // Add other files you want to cache here
-];
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js");
 
-// Install event
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+workbox.core.setCacheNameDetails({prefix: "control-dinero"});
+workbox.precaching.precacheAndRoute([
+  {url: "/", revision: null},
+  {url: "/index.html", revision: null},
+  {url: "/manifest.json", revision: null},
+  {url: "/sw.js", revision: null},
+  {url: "/Icono de calculadora_20250608_150126_0000.jpg", revision: null},
+  // Agrega aquí más archivos si los tienes, por ejemplo, CSS, JS, imágenes
+]);
 
-// Fetch event
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'script' ||
+                 request.destination === 'style' ||
+                 request.destination === 'image',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'static-resources',
+    plugins: [new workbox.expiration.ExpirationPlugin({maxEntries: 50})]
+  })
+);
+
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'document',
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'pages',
+    plugins: [new workbox.expiration.ExpirationPlugin({maxEntries: 10})]
+  })
+);
+
+// Actualización inmediata cuando hay cambios
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
